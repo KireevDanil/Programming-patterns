b47769b5826b65f3c0f35a6c01cdf78ed245bd95
KireevDanil <146985639+KireevDanil@users.noreply.github.com>
2024-11-26 12:07:58 +0300

    Task3 Избавился от сеттеров,геттеров. Создал конструктор с параметрами по умолчанию

--- Lab1/Student.kt
+++ Lab1/Student.kt
@@ -1,55 +1,18 @@
 class Student(
-    private var id: Int,
-    private var lastName: String,
-    private var firstName: String,
-    private var middleName: String?,
-    private var phone: String? = null,
-    private var telegram: String? = null,
-    private var email: String? = null,
-    private var git: String? = null
+    var id: Int,
+    var lastName: String,
+    var firstName: String,
+    var middleName: String?,
+    var phone: String? = null,
+    var telegram: String? = null,
+    var email: String? = null,
+    var git: String? = null
 ) {
-    constructor(id: Int, lastName: String, firstName: String, middleName: String) : this(
-        id, lastName, firstName, middleName, null, null, null, null
-    )
-    fun getId() = id
-    fun setId(value: Int) {
-        id = value
+    init {
+        // Проверяем обязательные поля
+        require(lastName.isNotBlank() && firstName.isNotBlank()) { "Фамилия и имя не могут быть пустыми" }
     }
 
-    fun getLastName() = lastName
-    fun setLastName(value: String) {
-        lastName = value
-    }
-
-    fun getFirstName() = firstName
-    fun setFirstName(value: String) {
-        firstName = value
-    }
-
-    fun getMiddleName() = middleName
-    fun setMiddleName(value: String?) {
-        middleName = value
-    }
-
-    fun getPhone() = phone
-    fun setPhone(value: String?) {
-        phone = value
-    }
-
-    fun getTelegram() = telegram
-    fun setTelegram(value: String?) {
-        telegram = value
-    }
-
-    fun getEmail() = email
-    fun setEmail(value: String?) {
-        email = value
-    }
-
-    fun getGit() = git
-    fun setGit(value: String?) {
-        git = value
-    }
     fun printInfo() {
         println(
             """
