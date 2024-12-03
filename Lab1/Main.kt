f14a54d143d85b5914f580ab015db74c45fa1feb
KireevDanil <146985639+KireevDanil@users.noreply.github.com>
2024-12-03 12:41:26 +0300

    Task 6 Добавил механизм обработки ошибок при вводе неправильного номера телефона

--- Lab1/Main.kt
+++ Lab1/Main.kt
@@ -1,35 +1,43 @@
 fun main() {
+    try {
+        val student1 = Student(
+            id = 1,
+            lastName = "Киреев",
+            firstName = "Данил",
+            middleName = "Максимович",
+            phone = "+79184444444",
+            telegram = "@danil",
+            email = "kireev@gmail.com",
+            git = "github.com/kireev"
+        )
+        student1.printInfo()
+    }catch (e: IllegalArgumentException){
+        println("Ошибка: ${e.message}")
+    }
+    try {
+        val student2 = Student(
+            id = 2,
+            lastName = "Петров",
+            firstName = "Петр",
+            middleName = "Петрович",
+            phone = "89604567101"
+        )
+        student2.printInfo()
+    }catch (e: IllegalArgumentException){
+        println("Ошибка: ${e.message}")
+    }
 
-    val student1 = Student(
-        id = 1,
-        lastName = "Киреев",
-        firstName = "Данил",
-        middleName = "Максимович",
-        phone = "89184444444",
-        telegram = "@danil",
-        email = "kireev@gmail.com",
-        git = "github.com/kireev"
-    )
-    val student2 = Student(
-        id = 2,
-        lastName = "Петров",
-        firstName = "Петр",
-        middleName = "Петрович",
-        phone = "89604567101"
-    )
-    val student3 = Student(
-        id = 3,
-        lastName = "Сидоров",
-        firstName = "Алексей",
-        middleName = "Сергеевич",
-        telegram = "@sidorov",
-        git = "github.com/sidorov"
-    )
-
-
-    // Вывод информации о каждом студенте
-    println("Информация о студентах:")
-    student1.printInfo()
-    student2.printInfo()
-    student3.printInfo()
+    try {
+        val student3 = Student(
+            id = 3,
+            lastName = "Сидоров",
+            firstName = "Алексей",
+            middleName = "Сергеевич",
+            phone = "+78005553535",
+            telegram = "@sidorov"
+        )
+        student3.printInfo()
+    }catch (e: IllegalArgumentException){
+        println("Ошибка: ${e.message}")
+    }
 }
