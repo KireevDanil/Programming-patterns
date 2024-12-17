5f9706ef5a8a4da40a77fee69316ad5de4ef5b2b
KireevDanil <146985639+KireevDanil@users.noreply.github.com>
2024-12-17 14:07:07 +0300

    Task 3 Проверка getInfo()

--- Lab2/Main.kt
+++ Lab2/Main.kt
@@ -1,17 +1,19 @@
 fun main() {
-    val validString = "Киреев Данил Максимович|21|github.com/kireev"
-    val invalidString = "Киреев Данил Максимович|210|github.com/kireev"
+    val validString = "Киреев Данил Максимович|21|github.com/kireev|+79186666666"
+    val invalidString = "Иванов Иван Иванович|21|github.com/kireev|+796086634122"
 
     try {
         val student1 = Student(validString)
         println("Студент успешно создан $student1")
+        println("Инфа: ${student1.getInfo()}")
     } catch (e: Exception) {
         println("Ошибка: ${e.message}")
     }
 
     try {
-        val person = Student(invalidString)
-        println("Студент успешно создан: $person")
+        val student2 = Student(invalidString)
+        println("Студент успешно создан: $student2")
+        println("Info: ${student2.getInfo()}")
     } catch (e: ParsingException) {
         println("Ошибка парсинга: ${e.message}")
     } catch (e: ValidationException) {
