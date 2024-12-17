cdbb6dea164c81937c853a9d5d79d2359d943ece
KireevDanil <146985639+KireevDanil@users.noreply.github.com>
2024-12-17 14:06:31 +0300

    Task 3 Добавил метод getInfo()

--- Lab2/Student.kt
+++ Lab2/Student.kt
@@ -4,27 +4,48 @@ class ParsingException(message: String) : Exception(message)
 
 class ValidationException(message: String) : Exception(message)
 
-class Student(val name: String, val age: Int, val git: String) {
-
+class Student(val name: String, val age: Int, val git: String, val phone: String) {
+    
     override fun toString(): String {
-        return "$name|$age|$git"
+        return "$name|$age|$git|$phone"
     }
-
+    
     constructor(data: String) : this(
         name = parseName(data),
         age = parseAge(data),
-        git = parseGit(data)
+        git = parseGit(data),
+        phone = parsePhone(data)
     ) {
         validate()
     }
-
+    
     private fun validate() {
         if (name.isBlank()) throw ValidationException("Имя не должно быть пустым")
         if (age !in 1..100) throw ValidationException("Значение возраста должно быть от 1 до 100")
         if (!git.contains(Regex("^github\\.com/[a-zA-Z0-9_-]+/?$"))) throw ValidationException("Неверный формат гита")
+        if(!phone.contains(Regex("^\\+7\\d{10}$"))) throw ValidationException("Неверный формат номера телефона")
+    }
+    // Метод для получения краткой информации о студенте
+    fun getInfo(): String {
+        return "${getShortName()} | ${fetchGit()} | ${getPhoneInfo()}"
+    }
+    //Фамилия и инициалы
+    private fun getShortName(): String {
+        val parts = name.split(" ")
+        val surname = parts.getOrNull(0) ?: "Unknown"
+        val initials = parts.drop(1).joinToString("") { it.take(1).uppercase() + "." }
+        return "$surname $initials"
     }
 
+    private fun fetchGit(): String {
+        return git
+    }
+ 
+    private fun getPhoneInfo(): String {
+        return phone
+    }
     companion object {
+
         private fun parseName(data: String): String {
             return data.split("|").getOrNull(0)?.trim()
                 ?: throw ParsingException("Ошибка парсинга имени")
@@ -38,6 +59,11 @@ class Student(val name: String, val age: Int, val git: String) {
         private fun parseGit(data: String): String {
             return data.split("|").getOrNull(2)?.trim()
                 ?: throw ParsingException("Ошибка парсинга гита")
+        }
+
+        private fun parsePhone(data: String): String {
+            return data.split("|").getOrNull(3)?.trim()
+                ?: throw ParsingException("Ошибка парсинга телефона")
         }
 
     }
