40f200cca903cae4add9ab1ca42671bd2c99bed5
KireevDanil <146985639+KireevDanil@users.noreply.github.com>
2024-12-23 22:25:42 +0300

    Task 8 Протестировал метод read_from_txt

--- Lab2/Main.kt
+++ Lab2/Main.kt
@@ -1,10 +1,16 @@
+import java.io.IOException
 fun main() {
-    val student = Student(1, "Иванов Иван Иванович", "github.com/ivan", "+79181231212")
-    println(student) 
+    val filePath = "students.txt"
 
-    val shortStudent = Student_short(student)
-    println(shortStudent) 
-
-    val shortStudent2 = Student_short(2, "Петров П.В.|github.com/petr|+79603213485")
-    println(shortStudent2) 
+    try {
+        val students = Student.read_from_txt(filePath)
+        println("Список студентов:")
+        students.forEach { println(it) }
+    } catch (e: IOException) {
+        println("Ошибка при работе с файлом: ${e.message}")
+    } catch (e: ParsingException) {
+        println("Ошибка при парсинге данных: ${e.message}")
+    } catch (e: ValidationException) {
+        println("Ошибка валидации данных: ${e.message}")
+    }
 }
