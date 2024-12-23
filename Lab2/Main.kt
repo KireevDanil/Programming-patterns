04f438cffdae7aeb871df41f1644cacc61932ba9
KireevDanil <146985639+KireevDanil@users.noreply.github.com>
2024-12-23 22:31:53 +0300

    Task 10 Тестирование

--- Lab2/Main.kt
+++ Lab2/Main.kt
@@ -2,10 +2,19 @@ import java.io.IOException
 fun main() {
     val filePath = "students.txt"
 
+    val students = listOf(
+        Student(1, "Иванов Иван Иванович", "github.com/ivan", "+71234567890"),
+        Student(2, "Петров Петр Петрович", "github.com/petr", "+79876543210")
+    )
+
     try {
-        val students = Student.read_from_txt(filePath)
-        println("Список студентов:")
-        students.forEach { println(it) }
+        Student.write_to_txt(filePath, students)
+        println("Данные успешно записаны в файл.")
+
+        val studentsFromFile = Student.read_from_txt(filePath)
+        println("Данные, считанные из файла:")
+
+        studentsFromFile.forEach { println(it.getShortInfo()) }
     } catch (e: IOException) {
         println("Ошибка при работе с файлом: ${e.message}")
     } catch (e: ParsingException) {
