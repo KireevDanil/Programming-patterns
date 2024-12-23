23a63c8ddab75ef700b4aa9311d351ee9b3473e4
KireevDanil <146985639+KireevDanil@users.noreply.github.com>
2024-12-23 22:24:43 +0300

    Task 7 Добавил метод read_from_txt

--- Lab2/Student.kt
+++ Lab2/Student.kt
@@ -1,3 +1,6 @@
+import java.io.File
+import java.io.IOException
+
 class ParsingException(message: String) : Exception(message)
 class ValidationException(message: String) : Exception(message)
 
@@ -25,6 +28,30 @@ class Student(ID: Int,
             val surname = parts[0]
             val initials = parts.drop(1).joinToString("") { it.take(1).uppercase() + "." }
             return "$surname $initials"
+        }
+        fun read_from_txt(filePath: String): Array<Student> {
+            val file = File(filePath)
+            if (!file.exists() || !file.isFile) {
+                throw IOException("Неверный путь или файл не существует")
+            }
+
+            val students = mutableListOf<Student>()
+            try {
+                file.forEachLine { line ->
+                    val data = line.split(",") 
+                    if (data.size != 4) throw ParsingException("Неверное количество данных в строке: $line")
+
+                    val ID = data[0].toIntOrNull() ?: throw ParsingException("Неверный формат ID: ${data[0]}")
+                    val name = data[1]
+                    val git = data[2]
+                    val phone = data[3]
+                    students.add(Student(ID, name, git, phone))
+                }
+            } catch (e: Exception) {
+                throw ParsingException("Ошибка при чтении или парсинге файла: ${e.message}")
+            }
+
+            return students.toTypedArray()
         }
 
     }
