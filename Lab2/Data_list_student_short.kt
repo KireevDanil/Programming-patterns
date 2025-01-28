7488030a5c972d92d8463f7cfecb723dd4922e0d
KireevDanil <146985639+KireevDanil@users.noreply.github.com>
2025-01-28 16:07:34 +0300

    ЛР2 Ч2 З7 реализовал переопределение методов в наследнике

--- Lab2/Data_list_student_short.kt
+++ Lab2/Data_list_student_short.kt
@@ -1,18 +1,12 @@
 class Data_list_student_short(elements: List<Student_short>) :
     Data_list<Student_short>(elements) {
 
-    // Переопределяем метод get_names
     override fun get_names(): List<String> {
         return listOf("Фамилия", "Git", "Контакт")
     }
 
-    // Переопределяем метод get_data
-    override fun get_data(): Data_table<Any> {
-        val tableData = elements.mapIndexed { index, student ->
-            // Генерируем строку для Data_table: индекс, Фамилия, Git, контакт
-            listOf(index + 1, student.surnameInitials, student.git, student.contact)
-        }
-        // Возвращаем объект Data_table с подготовленными данными
-        return Data_table.create(tableData)
+    // Извлечение атрибутов из объекта Student_short
+    override fun extractAttributes(element: Student_short): Array<Any> {
+        return arrayOf(element.surnameInitials, element.git, element.contact)
     }
 }
