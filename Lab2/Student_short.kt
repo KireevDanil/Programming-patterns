c57fd62d1d7d3348c4cf8c1651767090a6d77c60
KireevDanil <146985639+KireevDanil@users.noreply.github.com>
2025-01-28 14:47:46 +0300

    ЛР2 Ч2 З5 Модифицирован Student_short

--- Lab2/Student_short.kt
+++ Lab2/Student_short.kt
@@ -1,4 +1,4 @@
-class Student_short:StudentBase {
+class Student_short : StudentBase, Comparable<Student_short> {
     constructor(student: Student) : super(
         ID = student.ID,
         surnameInitials = student.surnameInitials,
@@ -12,6 +12,16 @@ class Student_short:StudentBase {
         git = parseData(data, 1),
         contact = parseData(data, 2)
     )
+
+    override fun compareTo(other: Student_short): Int {
+        return if (this.surnameInitials > other.surnameInitials) {
+            1
+        } else if (this.surnameInitials == other.surnameInitials) {
+            0
+        } else {
+            -1
+        }
+    }
 
     companion object {
         private fun parseData(data: String, index: Int): String {
