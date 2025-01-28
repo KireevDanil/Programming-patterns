f5c52c775551dfd42a7b8766a7636e0f921134c7
KireevDanil <146985639+KireevDanil@users.noreply.github.com>
2025-01-28 14:03:23 +0300

    ЛР2 Ч2 З4 Класс Data_list модифицирован

--- Lab2/Data_list.kt
+++ Lab2/Data_list.kt
@@ -1,18 +1,30 @@
-class Data_list<T : Comparable<T>> private constructor(private val elements: List<T>) {
+abstract class Data_list<T : Comparable<T>>(protected val elements: List<T>) {
+    private val selectedIndices = mutableSetOf<Int>()
+    init {
+        elements.sorted()
+    }
+    
+    // Выделить элемент по номеру
+    fun select(number: Int) {
+        require(number in elements.indices) { "Индекс выходит за пределы списка" }
+        selectedIndices.add(number)
+    }
+
+    // Получить массив id выделенных элементов
+    fun get_selected(): List<Int> = selectedIndices.toList()
+
+    // Получить массив наименований атрибутов, кроме ID
+    abstract fun get_names(): List<String>
+
+    // Получить объект класса Data_table
+    abstract fun get_data(): Data_table<Any>
 
     fun getElement(index: Int): T {
         require(index in elements.indices) { "Индекс выходит за пределы списка" }
         return elements[index]
     }
-    // Получение кол-ва элементов
-    fun getSize(): Int = elements.size
-    // Получение списка
+
     fun toList(): List<T> = elements.toList()
 
-    companion object {
-        fun <T : Comparable<T>> create(elements: List<T>): Data_list<T> {
-            return Data_list(elements.sorted())
-        }
-    }
-
+    fun getSize(): Int = elements.size
 }
